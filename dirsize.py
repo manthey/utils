6911dcfaaac04ff6c10e79266e48795eb61f9a9c
David Manthey <manthey@orbitals.com>
2021-08-28 21:19:08 -0400

    Update dirsize.

--- dirsize.py
+++ dirsize.py
@@ -11,33 +11,33 @@ def format_number(val):
      and a two letter suffix.
     Enter: val: a number to express as bytes.
     Exit:  formatted_val: the formatted value."""
-    factors = " kMGTP"
+    factors = ' kMGTP'
     if val < 100000:
-        return "%5d " % val
+        return '%5d ' % val
     for pos in range(len(factors)):
         suffix = factors[pos].strip()
-        frm = "%5.3f" % (float(val) / (1 << (pos * 10)))
-        d = frm.find(".")
+        frm = '%5.3f' % (float(val) / (1 << (pos * 10)))
+        d = frm.find('.')
         if d < 0 or d > 3:
             continue
         return frm[:5]+suffix
-    return frm.split(".")[0]+suffix
+    return frm.split('.')[0]+suffix
 
 
-if __name__ == "__main__":  # noqa
+if __name__ == '__main__':  # noqa
     bases = []
     help = False
     depth = 1
-    sort = "name"
+    sort = 'name'
     for pos in range(1, len(sys.argv)):
         arg = sys.argv[pos]
-        if arg.startswith("--depth="):
-            depth = int(arg.split("=")[1])
-        elif arg == "--size":
-            sort = "len"
-        elif arg == "-v":
+        if arg.startswith('--depth='):
+            depth = int(arg.split('=')[1])
+        elif arg == '--size':
+            sort = 'len'
+        elif arg == '-v':
             Verbose += 1
-        elif not arg.startswith("-") and not arg.startswith("/"):
+        elif not arg.startswith('-') and not arg.startswith('/'):
             bases.append(arg)
         else:
             help = True
@@ -55,38 +55,38 @@ If no directory is specified, the current directory is used.
 -v increases verbosity.""")
         sys.exit(0)
     if not len(bases):
-        bases.append(".")
+        bases.append('.')
     for base in bases:
         list = {}
         absroot = os.path.abspath(base)
         absroot = absroot.rstrip('"')
         if len(absroot) > 1:
-            absroot = absroot.rstrip("\\")
+            absroot = absroot.rstrip('\\')
         orig_absroot = absroot + ('\\' if absroot[-1:] == ':' else '')
-        rootlen = len(absroot.replace("\\", "/").split("/"))
-        for root, dirs, files in os.walk(orig_absroot, True):
+        rootlen = len(absroot.replace('\\', '/').split('/'))
+        for root, _dirs, files in os.walk(orig_absroot, True):
             if Verbose >= 1:
                 print(root)
             for file in files:
                 path = os.path.abspath(os.path.join(base, root, file))
                 if Verbose >= 2:
-                  print(path)
-                pos = len("/".join(path.replace("\\", "/").split(
-                    "/")[:rootlen + depth]))
+                    print(path)
+                pos = len('/'.join(path.replace('\\', '/').split(
+                    '/')[:rootlen + depth]))
                 key = path[len(absroot) + 1:pos]
                 if key not in list:
                     list[key] = {
-                        "base": base,
-                        "name": key,
-                        "root": path[len(absroot) + 1:pos],
-                        "len": 0
+                        'base': base,
+                        'name': key,
+                        'root': path[len(absroot) + 1:pos],
+                        'len': 0
                     }
                 try:
                     flen = os.path.getsize(path)
-                    list[key]["len"] += flen
+                    list[key]['len'] += flen
                 except Exception:
                     pass
         keys = [skey for (sval, skey) in sorted(
             [(list[lkey][sort], lkey) for lkey in list])]
         for key in keys:
-            print(format_number(list[key]["len"]), key if depth else absroot)
+            print('%s %s' % (format_number(list[key]['len']), key if depth else absroot))
