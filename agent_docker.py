80bf0c813e678ad03deab67a591ca70b55b24dcb
David Manthey <david.manthey@kitware.com>
2026-08-24 08:48:17 -0400

    Add config file

--- agent_docker.py
+++ agent_docker.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # /// script
 # requires-python = '>=3.10'
-# dependencies = []
+# dependencies = [
+#   'pyyaml',
+# ]
 # ///
 
 import argparse
@@ -11,10 +13,13 @@ import os
 import platform
 import re
 import shutil
+import socket
 import subprocess
 import sys
 import tarfile
 import tempfile
+
+import yaml
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
@@ -96,6 +101,37 @@ def get_mount_args(is_windows, more=None):
     return args
 
 
+def free_port(start, limit=50):
+    for port in range(start, start + limit):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            try:
+                s.bind(('0.0.0.0', port))
+                return port
+            except OSError:
+                continue
+    return start
+
+
+def add_config(args, src, parser):
+    if not os.path.exists(args.config):
+        return args
+    try:
+        config = yaml.safe_load(open(args.config))
+    except Exception as exc:
+        print('Failed to parse config file', exc)
+        return args
+    args = sys.argv[2:]
+    try:
+        if src in config:
+            args = config[src] + args
+        if 'all' in config:
+            args = config['all'] + args
+        return parser.parse_args(sys.argv[1:2] + args)
+    except Exception as exc:
+        print('Failed to merge config file', args, exc)
+        return args
+
+
 def main():  # noqa
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -112,19 +148,31 @@ def main():  # noqa
     parser.add_argument(
         '--fuse', action='store_true',
         help='Pass options to allow fuse to work when starting a container.')
+    parser.add_argument('--no-fuse', dest='fuse', action='store_false')
     parser.add_argument(
         '--gpu', '--gpus', action='store_true',
         help='Enable gpu access when starting a container.')
+    parser.add_argument('--no-gpu', '--no-gpus', dest='gpu', action='store_false')
     parser.add_argument(
         '--ssh',
-        help='Add the specified public key for the ubuntu user and expose '
-        'port 2222.')
+        help='Add the specified public key for the ubuntu user and expose a '
+        'port.')
+    parser.add_argument(
+        '--sshport', default=2222, type=int,
+        help='The port to expose for ssh. Default %(default)s')
+    parser.add_argument(
+        '--sshfreeport', action='store_true',
+        help='If specified, find the first free port for ssh starting at the '
+        'specified port.')
+    parser.add_argument('--no-sshfreeport', dest='sshfreeport', action='store_false')
     parser.add_argument(
         '--local', action='store_true',
         help='Mount local utilities directories.  This removes some isolation.')
+    parser.add_argument('--no-local', dest='local', action='store_false')
     parser.add_argument(
         '--docker', action='store_true',
         help='Mount docker sock with appropriate permissions.')
+    parser.add_argument('--no-docker', dest='docker', action='store_false')
     parser.add_argument(
         '--mount', action='append', default=[],
         help='Mount a folder into the docker.  Recommended format is local:inside:ro')
@@ -136,16 +184,24 @@ def main():  # noqa
              'Use commas for multiple patterns in one --skip (e.g., ".venv,*.pyc"). '
              'Repeat --skip for separate groups.')
     parser.add_argument(
+        '--config',
+        default=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'agent_docker.yaml'),
+        help='Config file with keys as the working directories with arrays of '
+        'default arguments for this script. Default %(default)s')
+    parser.add_argument(
         '--verbose', '-v', action='count', default=0,
         help='Increase verbosity')
     args = parser.parse_args()
+    if args.src:
+        os.chdir(os.path.expanduser(args.src))
+    current_dir = os.path.basename(os.getcwd())
+    args = add_config(args, os.getcwd(), parser)
+
     logger.setLevel(max(1, logging.WARNING - args.verbose * 10))
     logger.addHandler(logging.StreamHandler(sys.stderr))
     logger.debug('Parsed arguments: %r', args)
     # add more commands: list, run <model> <text> --detach, check, log
-    if args.src:
-        os.chdir(os.path.expanduser(args.src))
-    current_dir = os.path.basename(os.getcwd())
+
     basename = args.name or f'agent_{safe_filename(current_dir)}'
     container_name = basename + (f'_{args.num}' if args.num is not None else '')
     is_windows = platform.system().lower() == 'windows'
@@ -184,7 +240,10 @@ def main():  # noqa
         if args.docker:
             other_opts.extend(['-v', '/var/run/docker.sock:/var/run/docker.sock'])
         if args.ssh:
-            other_opts.extend(['-p', '2222:2222'])
+            port = free_port(args.sshport)
+            other_opts.extend(['-p', f'{port}:2222'])
+            if port != args.sshport:
+                logger.warning('Using ssh port %s', port)
         cmd = docker_cmd + [
             'run', '-d', '--rm', '--name', container_name,
             '--add-host', f'host.docker.internal:{gateway}',
@@ -204,7 +263,6 @@ def main():  # noqa
                 if any(fnmatch.fnmatch(part, pat) for part in parts):
                     return None
             return tarinfo
-
         with tempfile.SpooledTemporaryFile() as fp:
             with tarfile.open(fileobj=fp, mode='w') as tf:
                 tf.add(os.path.join('..', current_dir), filter=tar_exclusion_filter,
